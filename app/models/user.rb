4f56d0aee76aab2125db32dbbb515bfe56a3d418
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-12 23:56:40 +0900

    ユーザー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,          presence: true
-  validates :family_name_kanji, presence: true
-  validates :first_name_kanji,  presence: true
-  validates :family_name_kana,  presence: true
-  validates :first_name_kana,   presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+    validates :family_name_kanji
+    validates :first_name_kanji
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
   validates :birth,             presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+
 end
