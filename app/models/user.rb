cbcd2190f3d0cb2c5cac34ea12bcf0f2d9c57e66
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-12 16:41:34 +0900

    usesモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,          presence: true
+  validates :family_name_kanji, presence: true
+  validates :first_name_kanji,  presence: true
+  validates :family_name_kana,  presence: true
+  validates :first_name_kana,   presence: true
+  validates :birth,             presence: true
 end
