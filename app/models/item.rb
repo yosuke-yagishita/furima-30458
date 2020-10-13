11cc64fba39a2d49e901f9e522a7b0142e8c2890
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-13 17:02:17 +0900

    Itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_type
   belongs_to_active_hash :delivery_day
   belongs_to_active_hash :prefecture
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_type_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+  validates :image, presence: true
+
 end
