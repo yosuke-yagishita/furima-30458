6637ad27444ef43590e367a6bb14e006fb47f719
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-13 18:26:17 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_type
   belongs_to_active_hash :delivery_day
   belongs_to_active_hash :prefecture
-  has_one_attached :image
+ 
+  validates :title, :explanation, :category, :condition,
+            :delivery_type, :delivery_day, :prefecture, :price,
+            :user, :image, 
+            presence: true
 
-  validates :title, presence: true
-  validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :delivery_type_id, presence: true
-  validates :delivery_day_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :price, presence: true
-  validates :user, presence: true
-  validates :image, presence: true
-
+  validates :category, :condition,:delivery_type,
+            :delivery_day, :prefecture,
+            numericality: { other_than: 1 }
+            
 end
