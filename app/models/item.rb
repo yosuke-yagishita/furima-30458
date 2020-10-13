b90b07edbff5d9d83d37f407baa112791f0a3a13
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-13 16:50:17 +0900

    ItemモデルとUserモデルのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_type
+  belongs_to_active_hash :delivery_day
+  belongs_to_active_hash :prefecture
 end
