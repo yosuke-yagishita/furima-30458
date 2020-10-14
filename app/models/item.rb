3007bd07557910ff8b864dc60290d4493e74e562
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-14 17:56:41 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
  
   validates :title, :explanation, :category, :condition,
-            :delivery_type, :delivery_day, :prefecture, :price,
+            :delivery_type, :delivery_day, :prefecture,
             :user, :image, 
             presence: true
 
   validates :category_id, :condition_id, :delivery_type_id,
             :delivery_day_id, :prefecture_id,
-            numericality: { other_than: 1 }
-            
+            numericality: { other_than: 1, message: 'Select' }
+
+  validates :price,
+    presence: true,
+    numericality: { greater_than_or_equal_to: 300,
+                    less_than_or_equal_to: 9999999,
+                    message: 'Out of setting range',
+                  }
+
 end
