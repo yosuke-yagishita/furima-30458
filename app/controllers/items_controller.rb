e8ac7f70e3a7d818d9a32a75dfe6ccf061b99c59
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-16 14:28:39 +0900

    コードレビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(id: 'DESC')
@@ -36,9 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    if current_user.id == @item.user_id
+    @item.destroy
     redirect_to root_path
+    end
   end
 
   private
