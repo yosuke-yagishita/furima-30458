944602129af4a01ec2368024bf47994aedcdf215
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-22 11:29:48 +0900

    FactoryBot誤記入修正

--- spec/factories/purchase_buyer_infos.rb
+++ spec/factories/purchase_buyer_infos.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :purchase_buyer_info do
     postal_code { '123-4567' }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
-    city { '東京都' }
+    city { '渋谷区' }
     address_line { '1-1' }
     building_name { 'ビルディング' }
     phone_num { '09012345678' }
