ff9bc03f4b50118be8f0c4edd221944c1e7afe8d
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-22 12:37:39 +0900

    コードレビュー修正#3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'buyer_infos/index'
   devise_for :users
   root to: "items#index"
-  resources :items, expect: :update do
+  resources :items, expect: :index do
     resources :purchases, only: [:index, :create]
   end
 end
