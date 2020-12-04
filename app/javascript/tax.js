889c3f8f142c964b049d5a1bf7a68505b80cc876
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-12-04 12:47:22 +0900

    javascript実行条件の実装

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,14 +1,16 @@
-function tax() {
-  const itemPrice = document.getElementById("item-price");
-  itemPrice.addEventListener("input", ()=> {
-    const rate = 0.1
-    const countTax = Math.round(itemPrice.value * 100 * rate / 100).toLocaleString();
-    const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = `${countTax}`;
-    const countProfit = Math.round(itemPrice.value - (itemPrice.value * 100 * rate / 100)).toLocaleString();
-    const profit = document.getElementById("profit");
-    profit.innerHTML = `${countProfit}`;
-  });
-}
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  function tax() {
+    const itemPrice = document.getElementById("item-price");
+    itemPrice.addEventListener("input", ()=> {
+      const rate = 0.1
+      const countTax = Math.round(itemPrice.value * 100 * rate / 100).toLocaleString();
+      const addTaxPrice = document.getElementById("add-tax-price");
+      addTaxPrice.innerHTML = `${countTax}`;
+      const countProfit = Math.round(itemPrice.value - (itemPrice.value * 100 * rate / 100)).toLocaleString();
+      const profit = document.getElementById("profit");
+      profit.innerHTML = `${countProfit}`;
+    });
+  }
 
-window.addEventListener("load", tax);
+  window.addEventListener("load", tax);
+}
